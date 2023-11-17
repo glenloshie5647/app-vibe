133a44e62786d899c7dea0fb5962dbc09c9690ba
glenloshie5647 <glenloshie5647@gmail.com>
2023-11-17 15:23:29 +0000

    Added: complexCode.js

---
+++ complexCode.js
@@ -0,0 +1,125 @@
+// Filename: complexCode.js
+// Content: Implementation of a binary search tree with various operations
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return true;
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      } else if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const minValue = this.findMinNode(node.right);
+      node.value = minValue.value;
+      node.right = this.removeNode(node.right, minValue.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  getRootNode() {
+    return this.root;
+  }
+
+  // Additional operations can be added here
+}
+
+// Usage example:
+
+const bst = new BinarySearchTree();
+bst.insert(12);
+bst.insert(8);
+bst.insert(18);
+bst.insert(5);
+bst.insert(10);
+bst.insert(15);
+bst.insert(22);
+
+console.log(bst.search(15)); // Output: true
+console.log(bst.search(17)); // Output: false
+
+bst.remove(18);
+console.log(bst.getRootNode()); // Output: Node { value: 12, left: Node { value: 8, ... }, right: Node { value: 22, ... } }
